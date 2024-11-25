198ec423920955735f593ac92ada90d4353e4a59
小城 <kojolumi@gamil.com>
2024-11-25 14:11:05 +0900

    test

--- header.php
+++ header.php
@@ -99,10 +99,32 @@
   <!-- front-page.phpの場合 -->
   <?php if (is_front_page()): ?>
     <section class="section-contents" id="keyvisual" style="margin-top: 100px;">
-      <img src="https://atrail.co.jp/wp-content/uploads/images/bg-section-keyvisual-02.jpg" alt="">
-      <div class="wrapper">
-        <h1 class="site-caption">コンテナで、かなえる。<br>自由を、つくる。</h1>
-        <img src="https://atrail.co.jp/wp-content/uploads/images/logo-white@2x.png" alt="" srcset="" class="logo-img">
+      <!-- カルーセル -->
+      <div id="carouselIndicators" class="carousel slide" data-bs-ride="carousel" style="position: relative;">
+        <div>
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://atrail.co.jp/wp-content/uploads/images/bg-section-keyvisual-01.webp" class="d-block w-100" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img src="https://atrail.co.jp/wp-content/uploads/images/bg-section-keyvisual-02.webp" class="d-block w-100" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img src="https://atrail.co.jp/wp-content/uploads/images/bg-section-keyvisual-03.webp" class="d-block w-100" alt="Third slide">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselIndicators" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselIndicators" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          </button>
+        </div>
       </div>
     </section>
     <!-- front-page.php以外のphpページの場合 -->
