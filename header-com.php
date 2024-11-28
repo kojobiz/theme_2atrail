eff058fe4fc549adef29c7b0ce5ddbc3c59d80b0
lumijoe <spacedesign63@gmail.com>
2024-11-28 21:52:18 +0900

    test

--- header-com.php
+++ header-com.php
@@ -121,8 +121,8 @@
             <div id="primary" class="content-area">
                 <main>
                     <div class="page-contents" style="padding-top: 0px;">
-                        <div class="page-head" style="max-height: 230px;">
-                            <img src="https://atrail.co.jp/wp-content/uploads/images/bg-section-company.webp" alt="" style="max-height: 230px; object-position: center 42%;">
+                        <div class="page-head" style="max-height: 330px;">
+                            <img src="https://atrail.co.jp/wp-content/uploads/images/bg-section-company.webp" alt="" style="max-height: 330px; object-position: center 42%;">
                             <div class="wrapper" style="height: 20%; position: absolute; bottom: 0; width: 20vw; border-radius: 0px 10px 0px 0px; color: #fff; background-color: rgb(25 83 56 / 78%); text-align: left; padding-left: 2%; bottom: 0!important; left:0!important; transform: translate(0%, 150%); padding-top: 6px; padding-bottom: 3px;">
                                 <span class="page-title" style="font-weight: 500; font-size: 2.2rem; margin-top: 0px!important; font-size: clamp(12px, 2vw, 18px); "><?php echo get_main_title(); ?></span>
                                 <h2 class="page-title-en" style="font-size: 1.2rem; font-style: italic; margin-top: -6px;"><?php echo get_main_en_title(); ?></h2>
